d5b2eb384aa7d4a432af0ef489c13c94b53b4ae3
melnicenkovadik <melnicenkovadik@gmail.com>
2026-04-16 23:54:43 +0200

    feat(core): re-export domain + application modules from index

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,1 +1,7 @@
 export const CORE_VERSION = '0.0.1';
+
+export * from './domain/locale';
+export * from './domain/user';
+export * from './application/ports/clock';
+export * from './application/ports/user-repo';
+export * from './application/upsert-user';
